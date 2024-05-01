e8db77bfeaf0b75f04a457410ceea18665ed117c
grimmal72 <jordanmalmgren@gmail.com>
2024-04-30 18:35:08 -0700

    Create endingSequence.py

---
+++ endingSequence.py
@@ -0,0 +1,5 @@
+def endingSequence():
+  #  This will trigger the counting of how many shots each person took.
+  return 0
+
+# Note: this is imported but is not actually in use right now. I'll use it when I've written the game loop.
