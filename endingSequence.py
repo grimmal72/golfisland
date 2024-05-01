a6f8b4070af182ebc78a5f25327e065f1ce76079
grimmal72 <jordanmalmgren@gmail.com>
2024-04-30 20:33:52 -0700

    Score system finalized.
    Just needs character ranking system, and for them to say something (depending on 1st, 2nd, 3rd), and some kind of general closing paragraph.
    
    But regardless, this part is sort of finished.

--- endingSequence.py
+++ endingSequence.py
@@ -1,5 +1,34 @@
-def endingSequence():
-  #  This will trigger the counting of how many shots each person took.
-  return 0
+
+def endingSequence(shots, par):
+  scoreNames = ["Hole-in-one!", "Albatross.", "Eagle.", "Birdie.", "Par.", "Bogey.", "Double Bogey...", "Triple Bogey...", "Worse than Triple Bogey... Basically, really bad!"]
+
+  def golfScore(shots, par):
+      if shots == 1:
+          return scoreNames[0]
+      elif shots == par - 3:
+          return scoreNames[1]
+      elif shots == par - 2:
+          return scoreNames[2]
+      elif shots == par - 1:
+          return scoreNames[3]
+      elif shots == par:
+          return scoreNames[4]
+      elif shots == par + 1:
+          return scoreNames[5]
+      elif shots == par + 2:
+          return scoreNames[6]
+      elif shots == par + 3:
+          return scoreNames[7]
+      else:
+          return scoreNames[8]
+
+  fancyScoreWord = golfScore(shots, par)
+  return fancyScoreWord
+
+# Example usage:
+shots = 4
+coursePar = 3
+result = endingSequence(shots, coursePar)
+print(result)
 
 # Note: this is imported but is not actually in use right now. I'll use it when I've written the game loop.
